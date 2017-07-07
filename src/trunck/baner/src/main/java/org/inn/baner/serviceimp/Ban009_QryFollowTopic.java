98c0f8fff01a224371bf58dc9b2a77ad5cf22621
zxywithal <zxywithal@163.com>
2017-07-07 14:39:52 +0800

    新增交易ban009

---
+++ src/trunck/baner/src/main/java/org/inn/baner/serviceimp/Ban009_QryFollowTopic.java
@@ -0,0 +1,43 @@
+package org.inn.baner.serviceimp;
+
+
+import com.ztkx.transplat.container.service.ServiceException;
+import com.ztkx.transplat.container.service.intface.BusinessService;
+import com.ztkx.transplat.container.util.ContextUtil;
+import com.ztkx.transplat.platformutil.context.CommonContext;
+import org.apache.log4j.Logger;
+import org.inn.baner.bean.User;
+import org.inn.baner.constant.Ban;
+import org.inn.baner.constant.enums.BErrorCode;
+import org.inn.baner.handler.data.UserData;
+
+/**
+ * 上传帖子
+ * @author zhangxiaoyun
+ * 2016-3-15 16:24:11
+ * <p>Company:ztkx</p>
+ */
+public class Ban009_QryFollowTopic implements BusinessService {
+
+	private Logger logger = Logger.getLogger(Ban009_QryFollowTopic.class);
+
+	@Override
+	public CommonContext service(CommonContext context) throws ServiceException {
+
+		String mobileno = context.getFieldStr(Ban.mobileno,CommonContext.SCOPE_GLOBAL);
+
+		logger.info("mobileno ["+mobileno+"]");
+
+		UserData userData  = null;
+		try {
+			userData = new UserData();
+
+			User user = userData.qryByMobile(mobileno);
+			context.setFieldStr(Ban.followtopic, user.getFollowtopic(), CommonContext.SCOPE_GLOBAL);
+		} catch (Exception e) {
+			ContextUtil.setErrorCode(BErrorCode.FAIL.code, context);
+			logger.error("buss service exec exception ",e);
+		}
+		return context;
+	}
+}
